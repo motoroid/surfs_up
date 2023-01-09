ff3e8111ec80809c79d395377771bd1409085e0e
motoroid <motoroid24@gmail.com>
2023-01-09 16:34:52 -0700

    deliverable 1 done

--- app.py
+++ app.py
@@ -13,17 +13,17 @@ Measurement = Base.classes.measurement
 Station = Base.classes.station
 session=Session(engine)
 app = Flask(__name__)
-#@app.route("/")
-#def welcome():
-#    return(
-#    '''
-#    Welcome to the Climate Analysis API!
-#    Available Routes:
-#    /api/v1.0/precipitation/
-#    /api/v1.0/stations
-#    /api/v1.0/tobs
-#    /api/v1.0/temp/start/end
-#    ''')
+@app.route("/")
+def welcome():
+    return(
+    '''
+    Welcome to the Climate Analysis API!
+    Available Routes:
+    /api/v1.0/precipitation/
+    /api/v1.0/stations
+    /api/v1.0/tobs
+    /api/v1.0/temp/start/end
+    ''')
 @app.route("/api/v1.0/precipitation")
 def precipitation():
    prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
@@ -31,3 +31,8 @@ def precipitation():
     filter(Measurement.date >= prev_year).all()
    precip = {date: prcp for date, prcp in precipitation}
    return jsonify(precip)
+@app.route("/api/v1.0/stations")
+def stations():
+    return
+
+    
